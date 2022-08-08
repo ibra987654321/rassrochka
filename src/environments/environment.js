b4b9e9a9e03f154682e6d88f28da49c18a222ae7
IOrozobaev <ibra987654321>
2022-08-08 18:17:00 +0600

    end fix

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -2,8 +2,8 @@ export const environment = {
   authExample: 'http://172.28.141.23:8075',
   authAPI: 'http://10.16.230.114:8076',
   exampleAPI: 'http://172.28.141.23:8008/api/v1',
-  propApi: 'http://10.16.230.114:8080/rassrochka/api/v1',
+  // propApi: 'http://10.16.230.114:8080/rassrochka/api/v1',
 
   // prodApi: 'http://176.126.164.43:81',
-  // propApi: 'http://176.126.164.43:81/rassrochka/api/v1',
+  propApi: 'http://176.126.164.43:81/rassrochka/api/v1',
 }
