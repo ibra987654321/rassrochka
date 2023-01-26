ca5a85fb11d1758b5a8dac546c744896890fd056
IOrozobaev <ibra987654321>
2023-01-26 14:01:11 +0600

    second client

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,8 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const environment = {
-  authAPI: 'http://176.126.166.91:86/rassrochka/api/v1',
-  testApi: 'http://10.16.230.114:8888/rassrochka/api/v1',
-  propApi: 'http://176.126.166.91:86/rassrochka/api/v1',
+  authAPI: 'http://176.126.166.124:86/rassrochka/api/v1',
+  propApi: 'http://176.126.166.124:86/rassrochka/api/v1',
   auth: 'http://176.126.164.43:85/rassrochka/api/v1',
-  // propApi: 'http://176.126.164.43:85/rassrochka/api/v1',
 }
